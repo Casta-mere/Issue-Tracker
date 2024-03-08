674bbb288ac85959e3c4f7bf43739d2c49145703
Casta-mere <castamerego@gmail.com>
2024-03-08 11:16:30 +0800

    Style the Navbar

--- issue-tracker/app/NavBar.tsx
+++ issue-tracker/app/NavBar.tsx
@@ -1,10 +1,14 @@
+"use client";
+import classNames from "classnames";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { AiFillBug } from "react-icons/ai";
 
 const NavBar = () => {
+  const currentPath = usePathname();
   const links = [
-    { label: "DashBoard", href: "/DashBoard" },
-    { label: "Issues", href: "/Issues" },
+    { label: "DashBoard", href: "/dashboard" },
+    { label: "Issues", href: "/issues" },
   ];
   return (
     <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
@@ -14,7 +18,11 @@ const NavBar = () => {
       <ul className="flex space-x-6">
         {links.map((link) => (
           <Link
-            className="text-zinc-500 hover:text"
+            className={classNames({
+              "text-zinc-900": link.href === currentPath,
+              "text-zinc-500": link.href !== currentPath,
+              "hover:text-zinc-800 transaition-colors": true,
+            })}
             key={link.label}
             href={link.href}
           >
