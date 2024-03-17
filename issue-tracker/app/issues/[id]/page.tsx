b154dfc8bfd175af48d037a92fb069636fcf4053
Casta-mere <castamerego@gmail.com>
2024-03-17 16:09:35 +0800

    Add Assign select component

--- issue-tracker/app/issues/[id]/page.tsx
+++ issue-tracker/app/issues/[id]/page.tsx
@@ -6,6 +6,7 @@ import EditIssueButton from "./EditIssueButton";
 import IssueDetails from "./IssueDetails";
 import { getServerSession } from "next-auth";
 import authOptions from "@/app/api/auth/AuthOptions";
+import AssigneeSelect from "./AssigneeSelect";
 
 interface Props {
   params: { id: string };
@@ -27,6 +28,7 @@ const IssueDeatilPage = async ({ params }: Props) => {
       {session && (
         <Box>
           <Flex direction="column" gap="3">
+            <AssigneeSelect />
             <EditIssueButton issueId={issue.id} />
             <DeleteIssueButton issueId={issue.id} />
           </Flex>
