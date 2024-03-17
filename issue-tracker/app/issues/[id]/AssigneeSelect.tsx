eea9b9b95780e9a11c2e2649f2b0c381f6a05d36
Casta-mere <castamerego@gmail.com>
2024-03-17 16:50:54 +0800

    Fetch data with React Query

--- issue-tracker/app/issues/[id]/AssigneeSelect.tsx
+++ issue-tracker/app/issues/[id]/AssigneeSelect.tsx
@@ -1,26 +1,31 @@
 "use client";
 import { User } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
+import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { Skeleton } from "@/app/components";
 
 const AssigneeSelect = () => {
-  const [users, setUsers] = useState<User[]>([]);
+  const {
+    data: users,
+    error,
+    isLoading,
+  } = useQuery<User[]>({
+    queryKey: ["users"], // 用于缓存的 key，在不同地方调用 useQuery 若 key 一样则不会重复获取
+    queryFn: () => axios.get<User[]>("/api/users").then((res) => res.data), // 用于获取数据的函数
+    staleTime: 60 * 1000, // 数据缓存多久
+    retry: 3, // 最多重复获取几次
+  });
+  if (error) return null;
+  if (isLoading) return <Skeleton />;
 
-  useEffect(() => {
-    const getUsers = async () => {
-      const { data } = await axios.get<User[]>("/api/users");
-      setUsers(data);
-    };
-    getUsers();
-  }, []);
   return (
     <Select.Root>
       <Select.Trigger placeholder="Assign..." />
       <Select.Content>
         <Select.Group>
           <Select.Label>Suggestions</Select.Label>
-          {users.map((user) => (
+          {users?.map((user) => (
             <Select.Item value={user.id} key={user.id}>
               {user.name}
             </Select.Item>
